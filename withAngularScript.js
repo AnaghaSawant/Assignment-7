1c9a4af1df67e06ca3e317fc8f0bff3e4c1b06d8
AnaghaSawant <anaghasawant1610@gmail.com>
2019-03-18 19:54:46 +0530

    apply color from array and onclick remove button

--- withAngularScript.js
+++ withAngularScript.js
@@ -1,18 +1,30 @@
 var app = angular.module('MyApp', [])
-  app.controller('MyController', function ($scope) {
+  app.controller('MyController', function ($scope, $window) {
     $scope.friends = [];
     $scope.isVisible = false;
 
+     $scope.colors = [
+      {name:'black'},
+      {name:'white'},
+      {name:'red'},
+      {name:'blue'},
+      {name:'yellow'}
+    ];
+    $scope.myColor = $scope.colors[1];
+
     $scope.ShowDiv = function () {
       $scope.isVisible = true;
+       $scope.addVisible = true;
     }
 
     $scope.SubmitValue = function()
     {
+
       var has = {
         value: 0,
         txt:$scope.textValue,
-        color: $scope.clr
+        color: $scope.colour
+
       }
       $scope.friends.push(has);
       Reset();
@@ -32,6 +44,21 @@ var app = angular.module('MyApp', [])
       $scope.isVisible = false;
       $scope.clr = '';
       $scope.textValue = '';
+      $scope.addVisible = false;
     }
+
+    $scope.ApplyColor = function(colour){
+      $scope.colour = colour;
+    }
+
+    $scope.removeButton = function(index){
+
+    // console.log(mine);
+  if ($window.confirm("Do you want to delete:")) {
+    //Remove the item from Array using Index.
+    $scope.friends.splice(index, 1);
+  }
+
+  }
 
 });
